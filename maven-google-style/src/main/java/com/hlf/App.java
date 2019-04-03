552fbd05502d63e4156a6315ee7478051aa2c851
howardfackrell <howard.fackrell@octanner.com>
2019-04-03 10:51:09 -0600

    initial commit

---
+++ maven-google-style/src/main/java/com/hlf/App.java
@@ -0,0 +1,27 @@
+package com.hlf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Pear");
+        fruits.add("Orange");
+        fruits.add("Blueberry");
+
+        fruits.stream()
+                .filter( fruit -> fruit.contains("a"))
+                .forEach(System.out::println);
+    }
+}
